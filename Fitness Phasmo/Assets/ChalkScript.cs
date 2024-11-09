51c1bc3b23951eaa00686a06a7d8262f03e0abd5
MishiMads <mnj-777@hotmail.com>
2024-11-09 20:31:47 +0100

    Jeg pusher assets

---
+++ Fitness Phasmo/Assets/ChalkScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ChalkScript : MonoBehaviour
+{
+    public GameObject chalk;
+    
+    
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
